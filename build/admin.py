3054d2b9c40833bc2a0b6bc2a3978c3bdf1d31c8
belekomuraliev <boke74@mail.ru>
2022-12-07 17:29:04 +0600

    first commit

---
+++ build/admin.py
@@ -0,0 +1,38 @@
+from django.contrib import admin
+from .models import *
+
+
+
+
+
+@admin.register(Block)
+class BlockAdmin(admin.ModelAdmin):
+    actions_on_top = False
+    actions_on_bottom = True
+    list_display = ('block_number', 'amount_entry', 'amount_apartments_floor', 'ammount_floors',
+                    'total_apartment', 'total_sum')
+    list_display_links = ('block_number',)
+
+
+    @admin.display(description='Общая колличеств квартир')
+    def total_apartment(self, obj):
+        return f'{obj.amount_apartments_floor  * obj.ammount_floors} квартир в блоке № {obj.block_number}'
+
+
+
+
+
+@admin.register(Client)
+class ClientAdmin(admin.ModelAdmin):
+    date_hierarchy = 'sale_date'
+    actions_on_top = False
+    actions_on_bottom = True
+    list_display = ( 'owner', 'sale_date', 'status', 'total_area', 'total_sum_aparment' )
+    search_fields = ('owner',)
+    ordering = ('sale_date',)
+    list_editable = ('sale_date',)
+
+
+
+
+
---
+++ build/models.py
@@ -0,0 +1,35 @@
+from django.db import models
+from django.contrib import admin
+
+class Block(models.Model):
+    block_number = models.IntegerField(verbose_name='Номер блока')
+    price_m = models.IntegerField(verbose_name='Стоимость за 1 кв. метр')
+    amount_entry = models.IntegerField(verbose_name='Колличество подъезда')
+    amount_apartments_floor = models.IntegerField(verbose_name='Колличество квартир на этаж')
+    ammount_floors = models.IntegerField(verbose_name='Колличество этажей', default=9)
+
+    def __str__(self):
+        return str(self.block_number)
+
+    @admin.display(description='общая сумма в Блоке')
+    def total_sum(self):
+
+        return self.price_m * self.amount_apartments_floor * self.ammount_floors * self.price_m
+
+class Client(models.Model):
+    owner = models.CharField(max_length=20, verbose_name='ФИО владельца')
+    sale_date = models.DateField(verbose_name='Дата продажи', null=True, blank=True)
+    status = models.CharField(max_length=50, choices=(
+        ('выкуп','Выкуп'), ('доконца','Выкуп не доконца'), ('расторнут','Расторгнуто'),('продан','Не прдано'),
+    ), verbose_name='Статус квартиры')
+    block = models.ForeignKey(Block, on_delete=models.CASCADE, related_name='rel_block', related_query_name='rel_client')
+    total_area = models.IntegerField(verbose_name='Общая площадь кв.м.')
+
+    def __str__(self):
+        return self.owner
+
+    @admin.display(description='Стоимость квартиры')
+    def total_sum_aparment(self):
+        return self.total_area * self.block.price_m
+
+
